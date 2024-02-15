33db066cc4c26e283c1c7b531535d46ee41a4c1f
ashishxcode <ashish.patel0903@gmail.com>
2024-02-15 11:02:23 +0530

    CI : fetching post based on user in profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-const Profile = () => {
-  return <div>Profile</div>;
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const [posts, setPosts] = useState([]);
+
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(`/api/users/${session.user.id}/posts`);
+        const data = await response.json();
+
+        console.log("data", data);
+        setPosts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (session?.user?.id) {
+      fetchPosts();
+    }
+  }, []);
+
+  const handleEdit = () => {};
+  const handleDelete = () => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to my profile"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import PromptCard from "./PromptCard";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name}&nbsp;</span>
+        Profile
+      </h1>
+      <p className="desc text-left">{desc}</p>
+
+      <div className="my-16 prompt_layout">
+        {data.map((item, index) => (
+          <PromptCard
+            key={index}
+            prompt={item}
+            handleEdit={() => handleEdit && handleEdit(item)}
+            handleDelete={() => handleDelete && handleDelete(item)}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
